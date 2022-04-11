968b9cd31a56f1d4bef53725bb8b149c117cc956
Ryan Daley <rpdaley@gmail.com>
2022-04-11 17:03:42 -0400

    Day0 work…

---
+++ sick-fits/frontend/pages/_app.js
@@ -0,0 +1,17 @@
+import nProgress from 'nprogress';
+import Router from 'next/router';
+import Page from '../components/Page';
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+}
