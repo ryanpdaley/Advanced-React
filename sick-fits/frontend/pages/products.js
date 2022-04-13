085b46e36033a82dc7a12d21feba079d19fb5f7a
Ryan Daley <rpdaley@gmail.com>
2022-04-13 14:35:17 -0400

    module 5 frontend pages

--- sick-fits/frontend/pages/products.js
+++ sick-fits/frontend/pages/products.js
@@ -1,3 +1,9 @@
-export default function ProductsPage() {
-  return <></>;
+import Products from '../components/Products';
+
+export default function OrderPage() {
+  return (
+    <div>
+      <Products />
+    </div>
+  );
 }
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,3 +1,9 @@
+import CreateProduct from '../components/CreateProduct';
+
 export default function SellPage() {
-  return <></>;
+  return (
+    <div>
+      <CreateProduct />
+    </div>
+  );
 }
