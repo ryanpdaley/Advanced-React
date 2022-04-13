32eea0e7c763eb6ee9d0b196c1f1e01cca16024a
Ryan Daley <rpdaley@gmail.com>
2022-04-13 14:34:40 -0400

    Module 5 - components

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -23,7 +23,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
   .sub-bar {
     display: grid;
@@ -40,11 +40,11 @@ export default function Header() {
             <Link href="/">Sick fits</Link>
           </Logo>
         </Link>
+        <Nav />
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
-      <Nav />
     </HeaderStyles>
   );
 }
