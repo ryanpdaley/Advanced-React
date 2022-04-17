f6960bb600b2924e54688f35a85035d3fa915a6d
Ryan Daley <rpdaley@gmail.com>
2022-04-17 12:31:34 -0400

    Module 8: backend

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -8,8 +8,10 @@ import {
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { CartItem } from './schemas/CartItem';
 import { insertSeedData } from './seed-data';
 import { SendPasswordResetEmail } from './lib/mail';
+import { extendGraphqlSchema } from './mutations';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -51,7 +53,9 @@ export default withAuth(
       User,
       Product,
       ProductImage,
+      CartItem,
     }),
+    extendGraphqlSchema,
     ui: {
       isAccessAllowed: ({ session }) =>
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
